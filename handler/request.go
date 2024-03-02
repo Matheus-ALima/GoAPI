2d10dca22020f0649bc65bb77c981640375d0118
Matheus Lima <char.matheus.lima@gmail.com>
2024-03-02 12:43:19 -0300

    feat: add request and response helpers

--- handler/request.go
+++ handler/request.go
@@ -1,16 +1,44 @@
 package handler
 
-import (
-	"github.com/Matheus-ALima/GoAPI.git/config"
-	"gorm.io/gorm"
-)
+import "fmt"
 
-var (
-	logger *config.Logger
-	db     *gorm.DB
-)
+func errParamRequired(name, typ string) error {
+	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
 
-func InitializeHandler() {
-	logger = config.GetLogger("handler")
-	db = config.GetSQLite()
+}
+
+//CreateOpening
+
+type CreateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *CreateOpeningRequest) Validate() error {
+	if r.Role == "" && r.Company == "" && r.Location == "" && r.Link == "" && r.Remote == nil && r.Salary <= 0 {
+		return fmt.Errorf("request body is empty")
+	}
+	if r.Role == "" {
+		return errParamRequired("role", "string")
+	}
+	if r.Company == "" {
+		return errParamRequired("company", "string")
+	}
+	if r.Location == "" {
+		return errParamRequired("location", "string")
+	}
+	if r.Link == "" {
+		return errParamRequired("link", "string")
+	}
+	if r.Remote == nil {
+		return errParamRequired("remote", "bool")
+	}
+	if r.Salary <= 0 {
+		return errParamRequired("salary", "int64")
+	}
+	return nil
 }
