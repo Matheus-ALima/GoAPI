f9054c2ae270a009f13b7f8bc8901656f18ce63a
Matheus Lima <char.matheus.lima@gmail.com>
2024-03-03 13:02:54 -0300

    feat: add updateOpening logic

--- handler/updateOppening.go
+++ handler/updateOppening.go
@@ -3,11 +3,50 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Matheus-ALima/GoAPI.git/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func UpdateOppeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "PUT Oppening",
-	})
+	request := UpdateOpeningRequest{}
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamRequired("id", "queryParameter").Error())
+		return
+	}
+	opening := schemas.Oppening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+
+	//update opening
+	if request.Role != "" {
+		opening.Role = request.Role
+	}
+	if request.Company != "" {
+		opening.Company = request.Company
+	}
+	if request.Location != "" {
+		opening.Location = request.Location
+	}
+	if request.Remote != nil {
+		opening.Remote = *request.Remote
+	}
+	if request.Link != "" {
+		opening.Link = request.Link
+	}
+	if request.Salary > 0 {
+		opening.Salary = request.Salary
+	}
+	sendSuccess(ctx, "update-opening", opening)
 }
